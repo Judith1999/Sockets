872ea75e047c293848538e11e33201e8ce50e49f
Elma <elma_judith99@hotmail.com>
2020-07-21 17:58:52 -0500

    Hice lo que viene en el vídeo 1, ya me daba el codigo de las pantallas así que solo agregué los sockets

---
+++ src/sockets/Cliente.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sockets;
+import javax.swing.*;
+/**
+ *
+ * @author ELMA
+ */
+public class Cliente {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MarcoCliente mimarco=new MarcoCliente();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
+class MarcoCliente extends JFrame{
+	
+	public MarcoCliente(){
+		
+		setBounds(600,300,280,350);
+				
+		LaminaMarcoCliente milamina=new LaminaMarcoCliente();
+		
+		add(milamina);
+		
+		setVisible(true);
+		}	
+	
+}
+
+class LaminaMarcoCliente extends JPanel{
+	
+	public LaminaMarcoCliente(){
+	
+		JLabel texto=new JLabel("CLIENTE");
+		
+		add(texto);
+	
+		campo1=new JTextField(20);
+	
+		add(campo1);		
+	
+		miboton=new JButton("Enviar");
+		
+		add(miboton);	
+		
+	}
+	
+		
+		
+	private JTextField campo1;
+	
+	private JButton miboton;
+	
+}
